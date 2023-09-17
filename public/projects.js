a576d4b01d5f31526f5377b54d65e5885cbbabb6
Mari Azh <Marzi.aghchay@gmail.com>
2023-09-18 00:26:13 +0100

    other project img

--- public/projects.js
+++ public/projects.js
@@ -7,30 +7,30 @@ const projects = [
     image: "img/por.jpeg",
     address: "https://github.com/MariAzhdari",
   },
-  // {
-  //   id: 2,
-  //   name: "HubPlanner",
-  //   description:
-  //     "A collaborative project was undertaken with my team for one month. HubPlanner is a fully functional platform that allows volunteers to effortlessly track their journeys and verify their attendance times with a single click. Developed using React JS, Express, CSS, and Postgres, this full-stack project aptly showcases the technical prowess and versatility of my team.",
-  //   image: "/src/assets/hubPlanner.png",
-  //   address: "https://github.com/MariAzhdari/finalProject-HubPlanner",
-  // },
-  // {
-  //   id: 3,
-  //   name: "Amazon",
-  //   description:
-  //     "The Amazon Project was created by React and also Firebase and CSS this project is fully responsive and functional.",
-  //   image: "/src/assets/amazon.png",
-  //   address: "https://github.com/MariAzhdari/amazon-project",
-  // },
-  // {
-  //   id: 4,
-  //   name: "Blog",
-  //   description:
-  //     "Design React blog app using functional React components and React Router Dom.",
-  //   image: "/src/assets/blog.png",
-  //   address: "https://github.com/MariAzhdari/react-blog",
-  // },
+  {
+    id: 2,
+    name: "HubPlanner",
+    description:
+      "A collaborative project was undertaken with my team for one month. HubPlanner is a fully functional platform that allows volunteers to effortlessly track their journeys and verify their attendance times with a single click. Developed using React JS, Express, CSS, and Postgres, this full-stack project aptly showcases the technical prowess and versatility of my team.",
+    image: "img/hubPlanner.png",
+    address: "https://github.com/MariAzhdari/finalProject-HubPlanner",
+  },
+  {
+    id: 3,
+    name: "Amazon",
+    description:
+      "The Amazon Project was created by React and also Firebase and CSS this project is fully responsive and functional.",
+    image: "img/amazon.png",
+    address: "https://github.com/MariAzhdari/amazon-project",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    description:
+      "Design React blog app using functional React components and React Router Dom.",
+    image: "img/blog.png",
+    address: "https://github.com/MariAzhdari/react-blog",
+  },
 ];
 
 export default projects;
